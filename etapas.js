4c10c8544ec1dde72c2f67d2039aa1d9994074cd
Talita Viana <talitasaraiva@outlook.com.br>
2023-12-15 15:16:24 -0300

    First commit

---
+++ etapas.js
@@ -0,0 +1,46 @@
+let etapas = [
+  {
+    titulo: 'VEREADOR',
+    numeros: 5,
+    candidatos: [
+      {
+        numero: '38111',
+        nome: 'Fulano de Tal',
+        partido: 'ABC',
+        fotos: [{ url: 'prefeita1.jpg', legenda: 'Vereador' }],
+      },
+      {
+        numero: '77222',
+        nome: 'Beltrano da Silva',
+        partido: 'DEFG',
+        fotos: [{ url: 'vice1.jpg', legenda: 'Vereador' }],
+      },
+    ],
+  },
+  {
+    titulo: 'PREFEITO',
+    numeros: 2,
+    candidatos: [
+      {
+        numero: '99',
+        nome: 'Ciclano',
+        partido: 'ABC',
+        vice: 'Cic',
+        fotos: [
+          { url: 'prefeita2.jpg', legenda: 'Prefeito' },
+          { url: 'vice2.jpg', legenda: 'Vice-Prefeito', small: true },
+        ],
+      },
+      {
+        numero: '84',
+        nome: 'Zulano',
+        partido: 'FOM',
+        vice: 'Lano',
+        fotos: [
+          { url: 'prefeita3.jpg', legenda: 'Prefeito' },
+          { url: 'vice3.jpg', legenda: 'Vice-Prefeito', small: true },
+        ],
+      },
+    ],
+  },
+];
